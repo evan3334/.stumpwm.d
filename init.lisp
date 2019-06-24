aba3694f549d0d4ea3ae38c33d4e95c2873d45e5
Evan Straw <evan.straw99@gmail.com>
2019-06-24 12:09:21 -0400

    Create laptop config

--- init.lisp
+++ init.lisp
@@ -1,9 +1,41 @@
+(load "/home/evan/quicklisp/setup.lisp")
+(ql:quickload :swank)
+
 (in-package :stumpwm)
-;; bruh
-(run-shell-command "ckb-next -b")
-(run-shell-command "/home/evan/bin/fixgraphics")
-(run-shell-command "/home/evan/bin/ctrlcapson")
-(run-commands "exec polybar example")
+
+(defvar *config-dir* "/home/evan/.stumpwm.d/" "StumpWM configuration directory")
+
+(defun load-user-module (name)
+  (load (make-pathname :defaults *config-dir*
+		       :name name
+		       :type "lisp")))
+
+(run-commands "exec /home/evan/bin/ctrlcapson")
+(run-commands "exec /home/evan/bin/touchpad.sh")
+(run-commands "exec synapse -s")
+(run-commands "exec /home/evan/Telegram/Telegram")
 (run-commands "exec keepassxc")
 (run-commands "exec xterm")
-(run-commands "exec synapse")
+
+(setf *mouse-focus-policy* :click)
+
+(setf stumpwm:*screen-mode-line-format*
+      (list "%W | "
+	    '(:eval (stumpwm:run-shell-command "date '+%a %b %d %H:%M %Z %Y'" t))))
+
+(setf stumpwm:*mode-line-timeout* 10)
+
+(stumpwm:toggle-mode-line (stumpwm:current-screen)
+			  (stumpwm:current-head))
+
+(swank-loader:init)
+(defcommand swank () ()
+	    (swank:create-server :port 4005
+				 :style swank:*communication-style*
+				 :dont-close t)
+	    (echo-string
+	     (current-screen)
+	     "Starting swank. M-x slime-connect RET RET, then (in-package stumpwm)."))
+(swank)
+
+
