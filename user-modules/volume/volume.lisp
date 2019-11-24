fb70cda2ca3e6a9614651c386c8ecee21542b869
Evan Straw <evan.straw99@gmail.com>
2019-11-23 23:03:04 -0800

    Add muting support to volume module

--- user-modules/volume/volume.lisp
+++ user-modules/volume/volume.lisp
@@ -39,16 +39,26 @@
    "\\* index:(?:.*[\\S\\s]{2}){1,10}.*\\s(\\d{1,3})%.*\\s(\\d{1,3})%"
    :multi-line-mode t))
 
+(defvar *mute-scanner*
+  (ppcre:create-scanner
+   "\\* index:(?:.*[\\s\\S]{2}){1,16}muted: ([a-z]{2,3})"
+   :multi-line-mode t))
+
 (defun parse-volume (str)
   (ppcre:register-groups-bind (lvolume rvolume)
        (*volume-scanner* str)
     (values (parse-integer lvolume) (parse-integer rvolume))))
 
+(defun parse-mute (str)
+  (ppcre:register-groups-bind (muted)
+      (*mute-scanner* str)
+    (string-equal muted "yes")))
+
 (defun get-volume ()
   (parse-volume (list-sinks)))
 
-(defun get-avg-volume ()
-  (multiple-value-bind (l r) (get-volume)
+(defun get-avg-volume (&key str)
+  (multiple-value-bind (l r) (if str (parse-volume str) (get-volume))
     (multiple-value-bind (q) (floor (+ l r) 2) q)))
 
 (defun build-volume-command (sink volume)
@@ -59,34 +69,39 @@
 	 (volstr (format nil "~a~a" operator absvol)))
     (format nil "pactl set-sink-volume ~a ~a% && echo ok" sink volstr)))
 
+(defun build-mute-command (sink)
+  (format nil "pactl set-sink-mute ~a toggle && echo ok" sink))
+
 (defun volume-pct-formatter (ml)
   (declare (ignore ml))
-  (let* ((vol (get-avg-volume))
+  (let* ((sinks (list-sinks))
+	 (muted (parse-mute sinks))
+	 (vol (get-avg-volume :str sinks))
 	 (capvol (if (> vol 100) 100 vol))
-	 (volbar (stumpwm:bar capvol 10 #\= #\-)))
-    (format nil "~a% [~a]" vol volbar)))
-
+	 (volstr (if muted "MUTE" (format nil "~a%" capvol)))
+	 (volbar (stumpwm:bar (if muted 0 capvol) (if muted 9 10) #\= #\-)))
+    (format nil "~a [~a]" volstr volbar)))
 (add-screen-mode-line-formatter #\V #'volume-pct-formatter)
 
-(defun inc-volume (pct)
+(defcommand volume-inc (pct) ((:number "Enter volume percent: "))
   (async-run (build-volume-command "@DEFAULT_SINK@" pct))
   (get-avg-volume))
 
-(defun dec-volume (pct)
-  (inc-volume (* -1 pct)))
+(defcommand volume-dec (pct) ((:number "Enter volume percent: "))
+  (volume-inc (* -1 pct)))
 
-(defcommand volume-inc (val) ((:number "Enter volume percent: "))
-  (inc-volume val))
-
-(defcommand volume-dec (val) ((:number "Enter volume percent: "))
-  (dec-volume val))
+(defcommand volume-mute () ()
+  (async-run (build-mute-command "@DEFAULT_SINK@"))
+  (if (parse-mute (list-sinks)) "Muted" "Unmuted"))
 
 (define-key *top-map* (kbd "XF86AudioRaiseVolume") "volume-inc 2")
 (define-key *top-map* (kbd "XF86AudioLowerVolume") "volume-dec 2")
 (define-key *top-map* (kbd "M-XF86AudioRaiseVolume") "volume-inc 5")
 (define-key *top-map* (kbd "M-XF86AudioLowerVolume") "volume-dec 5")
+(define-key *top-map* (kbd "XF86AudioMute") "volume-mute")
 
 (export '(get-volume
 	  get-avg-volume
-	  inc-volume
-	  dec-volume))
+	  volume-inc
+	  volume-dec
+	  volume-mute))
